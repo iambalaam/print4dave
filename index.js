7e39bb37636068949a1da6321036965c2e3700a5
iambalaam <guy.balaam@gmail.com>
2022-02-18 15:46:52 +0000

    Working with server data

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
 const { template } = require('./labelTemplate');
 const { print } = require('./print');
 
 const PORT = 8090;
 
 const app = express();
-app.use(express.json());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', async (req, res) => {
-    await print(template(req.body));
-    res.sendStatus(200);
+app.all('/', async (req, res) => {
+
+    try {
+        const json = JSON.parse(req.body.JSONString);
+        await print(template(json))
+        res.sendStatus(200);
+    } catch (e) {
+        res.status(500);
+        res.send(e);
+    }
+
 })
 
 app.listen(PORT, '0.0.0.0', () => {
